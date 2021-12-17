f48314be135f3b57609f993c32accb9a1f990bfa
Mikhail Brovkin <mabrovkin@gmail.com>
2021-12-17 21:59:51 +0300

    Add Lab4 WD Advanced

---
+++ HW4/Pages/CreateProductPage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Interactions;
+
+namespace HW4.Pages
+{
+    class CreateProductPage : AbstractPage
+    {
+        public CreateProductPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='ProductName']")]
+        private IWebElement ProductName;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='CategoryId']")]
+        private IWebElement CategoryId;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='SupplierId']")]
+        private IWebElement SupplierId;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='UnitPrice']")]
+        private IWebElement UnitPrice;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='QuantityPerUnit']")]
+        private IWebElement QuantityPerUnit;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='UnitsInStock']")]
+        private IWebElement UnitsInStock;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='UnitsOnOrder']")]
+        private IWebElement UnitsOnOrder;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='ReorderLevel']")]
+        private IWebElement ReorderLevel;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@class,'btn')]")]
+        private IWebElement SendBtn;
+
+        public void Create()
+        {
+            new SelectElement(CategoryId).SelectByText("Seafood");
+            new SelectElement(SupplierId).SelectByText("Heli Su?waren GmbH & Co. KG");
+            IAction action = new Actions(driver)
+                .Click(ProductName)
+                .SendKeys("BrovaTheOctopus")
+                .Click(UnitPrice)
+                .SendKeys("60")
+                .Click(QuantityPerUnit)
+                .SendKeys("1")
+                .Click(UnitsInStock)
+                .SendKeys("1")
+                .Click(UnitsOnOrder)
+                .SendKeys("1")
+                .Click(ReorderLevel)
+                .SendKeys("1")
+                .Click(SendBtn);
+            action.Perform();
+        }
+    }
+}
---
+++ HW4/UnitTest1.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using SeleniumExtras.PageObjects;
+using System;
+using HW4.Pages;
+
+namespace HW4
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://localhost:5000";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(2);
+
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Login("user", "user");
+            Assert.AreNotEqual(loginPage.GetNamePage(), "Login");
+        }
+
+        [Test]
+        public void AddProduct()
+        {
+            MenuPage menuPage = new MenuPage(driver);
+            AllProductsPage productsPage = menuPage.GoToProducts();
+            CreateProductPage product = productsPage.AddNewProduct();
+            product.Create();
+
+            Assert.AreEqual(productsPage.GetNamePage(), "All Products");
+        }
+        [Test]
+        public void CheckProduct()
+        {
+            MenuPage menuPage = new MenuPage(driver);
+            AllProductsPage productsPage = menuPage.GoToProducts();
+            EditProductPage product = productsPage.ViewProduct();
+            product.Check();
+        }
+
+        [Test]
+        public void RemoveProduct()
+        {
+            MenuPage menuPage = new MenuPage(driver);
+            AllProductsPage productsPage = menuPage.GoToProducts();
+            productsPage.Remove();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            MenuPage menuPage = new MenuPage(driver);
+            menuPage.Logout();
+            driver.Quit();
+        }
+    }
+}
